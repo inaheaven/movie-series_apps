14ebc6ba4ce850f0612efc462dac5750c11a666b
sangmin <sangmin.rim@gmail.com>
2020-02-05 00:53:11 +0900

    first commit

---
+++ src/APIKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+  APIKey: 'e524296c',
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import {HashRouter as Router, Route} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+import Landing from './components/home/Landing';
+import Movie from './components/home/Movie';
+
+import store from './store';
+
+class App extends Component {
+  render() {
+    return(
+      <Provider store={store}>
+        <Router>
+          <NavBar/>
+          <Route exact path='/' component={Landing}/>
+          <Route exact path='/movie/:id' component={Movie}/>
+          <Footer/>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
